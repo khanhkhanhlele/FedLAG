658d0fed97715975091c485ca8685dd8ecf61b4f
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-09-07 09:51:45 +0700

    a

--- system/flcore/clients/clientrecon.py
+++ system/flcore/clients/clientrecon.py
@@ -83,7 +83,7 @@ class clientRecon(Client):
         Get parameter-wise gradients. (weight and bias are not concatenated.)
         """
         grad_list = []
-        for name, param in self.network.parameters().items():
+        for name, param in self.model.parameters().items():
             grad = param.grad
             if grad is not None:
                 grad_cur = grad.data.detach().clone().view(-1)
