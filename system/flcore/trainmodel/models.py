a91f899a2f04be9f22167d37e5f41eecdc4e2bbc
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-09-24 15:49:45 +0700

    s

--- system/flcore/trainmodel/models.py
+++ system/flcore/trainmodel/models.py
@@ -248,48 +248,99 @@ class FedAvgCNN(nn.Module):
 #================================================================================================================
 #20 layers
 
-class FedAvgCNN20(nn.Module):
-    def __init__(self, in_channels=1, num_classes=10, dim=1024):
-        super().__init__()
-        
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(128 * 4 * 4, 512),
-            nn.ReLU(inplace=True)
-        )
-
-        self.fc2 = nn.Linear(512, num_classes)
-
+class CustomCNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CustomCNN, self).__init__()
+        
+        # Layer 1
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Layer 2
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Layer 3
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU()
+        
+        # Layer 4
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.relu4 = nn.ReLU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Layer 5
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.relu5 = nn.ReLU()
+        
+        # Layer 6
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.relu6 = nn.ReLU()
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Layer 7
+        self.conv7 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.relu7 = nn.ReLU()
+        
+        # Layer 8
+        self.conv8 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu8 = nn.ReLU()
+        self.maxpool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Layer 9 (Fully Connected)
+        self.fc1 = nn.Linear(512 * 4 * 4, 1024)
+        self.relu9 = nn.ReLU()
+        
+        # Layer 10 (Fully Connected)
+        self.fc2 = nn.Linear(1024, 512)
+        self.relu10 = nn.ReLU()
+        
+        # Output Layer (Fully Connected)
+        self.fc3 = nn.Linear(512, num_classes)
+        
     def forward(self, x):
         out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.maxpool1(out)
+        
         out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.maxpool2(out)
+        
         out = self.conv3(out)
+        out = self.relu3(out)
+        
+        out = self.conv4(out)
+        out = self.relu4(out)
+        out = self.maxpool3(out)
+        
+        out = self.conv5(out)
+        out = self.relu5(out)
+        
+        out = self.conv6(out)
+        out = self.relu6(out)
+        out = self.maxpool4(out)
+        
+        out = self.conv7(out)
+        out = self.relu7(out)
+        
+        out = self.conv8(out)
+        out = self.relu8(out)
+        out = self.maxpool5(out)
+        
         out = out.view(out.size(0), -1)
+        
         out = self.fc1(out)
+        out = self.relu9(out)
+        
         out = self.fc2(out)
+        out = self.relu10(out)
+        
+        out = self.fc3(out)
+        
         return out
 
 # ====================================================================================================================
