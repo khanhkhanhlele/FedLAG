5dd968db3a6b072d5464ed11cf786ca0ec4246cb
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-10-10 17:12:37 +0700

    s

--- system/flcore/clients/clientbase.py
+++ system/flcore/clients/clientbase.py
@@ -256,10 +256,10 @@ class Client(object):
         for model_idx, (params_model1, params_model2) in enumerate(zip(model.named_parameters(), self.model.named_parameters())):
             name_model1, param_model1 = params_model1
             name_model2, param_model2 = params_model2
-            print(name_model1+"----"+name_model2)
-            print("_____________________")
+            # print(name_model1+"----"+name_model2)
+            # print("_____________________")
             if name_model1 in layer:
-                none
+                None
                 #break
             param_model2 = params_model1
                 
