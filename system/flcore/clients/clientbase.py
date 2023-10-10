635336aef603f0402ad0217f6de486ed556608c1
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-10-10 20:35:20 +0700

    s

--- system/flcore/clients/clientbase.py
+++ system/flcore/clients/clientbase.py
@@ -259,6 +259,7 @@ class Client(object):
             if name_model1 in layer:
                 None
                 continue
+            print("name_model1")
             param_model2 = params_model1
             params_model2 = params_model1
         # for new_param, old_param in zip(model.parameters(), self.model.parameters()):
