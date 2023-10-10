3c561afb6c9462f86081f210c79c4fedda55ddf8
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-10-10 20:29:59 +0700

    s

--- system/flcore/clients/clientbase.py
+++ system/flcore/clients/clientbase.py
@@ -254,7 +254,6 @@ class Client(object):
         clone parameter from layer in list layer
         """
         for model_idx, (params_model1, params_model2) in enumerate(zip(model.named_parameters(), self.model.named_parameters())):
-            print("begin client")
             name_model1, param_model1 = params_model1
             name_model2, param_model2 = params_model2
             if name_model1 in layer:
