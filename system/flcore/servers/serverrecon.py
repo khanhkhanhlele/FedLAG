f0603e09723ba770cd826e3ca15d367aa7a3124b
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-10-12 16:23:01 +0700

    s

--- system/flcore/servers/serverrecon.py
+++ system/flcore/servers/serverrecon.py
@@ -32,7 +32,7 @@ class Recon(Server):
         self.s = args.s_score
         #self.sub_method = args.sub_method
         #self.mini_rounds = args.mini_rounds
-        self.mini_rounds = int(self.global_rounds/2)
+        self.mini_rounds = min(int(self.global_rounds/2),30)
         #self.mini_rounds = 30
         self.top_k = args.top_k
             
