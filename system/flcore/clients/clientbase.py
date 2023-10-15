ae9b1deda688c01f4b7c52ea440d48c10bb43b6f
Skydvn <mduongbkhn@gmail.com>
2023-10-15 15:18:42 +0900

    hot fix num_clients is None

--- system/flcore/clients/clientbase.py
+++ system/flcore/clients/clientbase.py
@@ -57,13 +57,13 @@ class Client(object):
     def load_train_data(self, batch_size=None):
         if batch_size == None:
             batch_size = self.batch_size
-        train_data = read_client_data(self.dataset, self.id, is_train=True)
+        train_data = read_client_data(self.dataset, self.id, is_train=True, num_clients=self.num_clients)
         return DataLoader(train_data, batch_size, drop_last=True, shuffle=False)
 
     def load_test_data(self, batch_size=None):
         if batch_size == None:
             batch_size = self.batch_size
-        test_data = read_client_data(self.dataset, self.id, is_train=False)
+        test_data = read_client_data(self.dataset, self.id, is_train=False, num_clients=self.num_clients)
         return DataLoader(test_data, batch_size, drop_last=False, shuffle=False)
         
     def set_parameters(self, model):
