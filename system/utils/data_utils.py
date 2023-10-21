f2a8768ae0d57054611f58be2579dc6679e6aac1
Skydvn <mduongbkhn@gmail.com>
2023-10-21 23:45:46 +0900

    update adaptive local data path

--- system/utils/data_utils.py
+++ system/utils/data_utils.py
@@ -3,9 +3,9 @@ import os
 import torch
 
 
-def read_data(dataset, idx, is_train=True, num_clients=None):
+def read_data(dataset, idx, niid, balance, alpha, is_train=True, num_clients=None):
     if is_train:
-        train_data_dir = os.path.join('../dataset', dataset, str(num_clients), 'train/')
+        train_data_dir = os.path.join('../dataset', dataset, f"{balance}_{niid}_{alpha}", 'train/')
 
         train_file = train_data_dir + str(idx) + '.npz'
         with open(train_file, 'rb') as f:
@@ -14,7 +14,7 @@ def read_data(dataset, idx, is_train=True, num_clients=None):
         return train_data
 
     else:
-        test_data_dir = os.path.join('../dataset', dataset, str(num_clients), 'test/')
+        test_data_dir = os.path.join('../dataset', dataset, f"{balance}_{niid}_{alpha}", 'test/')
 
         test_file = test_data_dir + str(idx) + '.npz'
         with open(test_file, 'rb') as f:
@@ -23,32 +23,32 @@ def read_data(dataset, idx, is_train=True, num_clients=None):
         return test_data
 
 
-def read_client_data(dataset, idx, is_train=True, num_clients=None):
+def read_client_data(dataset, idx, niid, balance, alpha, is_train=True, num_clients=None):
     if num_clients is None:
         raise ValueError("num_clients cannot be None")
     if dataset[:2] == "ag" or dataset[:2] == "SS":
-        return read_client_data_text(dataset, idx, is_train, num_clients)
+        return read_client_data_text(dataset, idx, niid, balance, alpha, is_train, num_clients)
     elif dataset[:2] == "sh":
-        return read_client_data_shakespeare(dataset, idx, num_clients)
+        return read_client_data_shakespeare(dataset, idx, niid, balance, alpha, is_train, num_clients)
 
     if is_train:
-        train_data = read_data(dataset, idx, is_train, num_clients)
+        train_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_train = torch.Tensor(train_data['x']).type(torch.float32)
         y_train = torch.Tensor(train_data['y']).type(torch.int64)
 
         train_data = [(x, y) for x, y in zip(X_train, y_train)]
         return train_data
     else:
-        test_data = read_data(dataset, idx, is_train, num_clients)
+        test_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_test = torch.Tensor(test_data['x']).type(torch.float32)
         y_test = torch.Tensor(test_data['y']).type(torch.int64)
         test_data = [(x, y) for x, y in zip(X_test, y_test)]
         return test_data
 
 
-def read_client_data_text(dataset, idx, is_train=True, num_clients=None):
+def read_client_data_text(dataset, idx, niid, balance, alpha, is_train=True, num_clients=None):
     if is_train:
-        train_data = read_data(dataset, idx, is_train, num_clients)
+        train_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_train, X_train_lens = list(zip(*train_data['x']))
         y_train = train_data['y']
 
@@ -59,7 +59,7 @@ def read_client_data_text(dataset, idx, is_train=True, num_clients=None):
         train_data = [((x, lens), y) for x, lens, y in zip(X_train, X_train_lens, y_train)]
         return train_data
     else:
-        test_data = read_data(dataset, idx, is_train, num_clients)
+        test_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_test, X_test_lens = list(zip(*test_data['x']))
         y_test = test_data['y']
 
@@ -71,16 +71,16 @@ def read_client_data_text(dataset, idx, is_train=True, num_clients=None):
         return test_data
 
 
-def read_client_data_shakespeare(dataset, idx, is_train=True, num_clients=None):
+def read_client_data_shakespeare(dataset, idx, niid, balance, alpha, is_train=True, num_clients=None):
     if is_train:
-        train_data = read_data(dataset, idx, is_train, num_clients)
+        train_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_train = torch.Tensor(train_data['x']).type(torch.int64)
         y_train = torch.Tensor(train_data['y']).type(torch.int64)
 
         train_data = [(x, y) for x, y in zip(X_train, y_train)]
         return train_data
     else:
-        test_data = read_data(dataset, idx, is_train, num_clients)
+        test_data = read_data(dataset, idx, niid, balance, alpha, is_train, num_clients)
         X_test = torch.Tensor(test_data['x']).type(torch.int64)
         y_test = torch.Tensor(test_data['y']).type(torch.int64)
         test_data = [(x, y) for x, y in zip(X_test, y_test)]
