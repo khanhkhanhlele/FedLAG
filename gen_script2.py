c1f30db6ff3c26e10c52ee6dc6a5985a4655e460
Skydvn <mduongbkhn@gmail.com>
2023-11-27 22:41:50 +0900

    update genscript

--- gen_script2.py
+++ gen_script2.py
@@ -6,7 +6,7 @@ import random
 import itertools
 
 if __name__ == "__main__":
-    sweep_path = "/7.2.3"
+    sweep_path = "/7.2.1" #change swepp_path for each experiment
     sweep_file = os.getcwd() + "/experiment" + sweep_path + ".json"
     
     sweep_data = json.load(open(sweep_file, mode='r'))
@@ -38,17 +38,11 @@ if __name__ == "__main__":
             
             run_file = script_folder + f"/{_ds['name']}_{_algo}.sh"
             
-            #noniid
             for _alpha, _nc, _model, _jr, _top_k, _sc in itertools.product(alpha, nc, _ds['-m'], join_ratio, top_k, sc):
                 cmd_lst.append(
-                    f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -data {_ds['name']} -nb {_ds['#cls']} -m {_model} -algo {_algo} -gr {nc[_nc]}  -jr {_jr} --top_k {_top_k} -sc {_sc} -bt 0.001 -go train -fceal --log --noniid --alpha_dirich {_alpha} -did {gpu}\n"
+                    f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -data {_ds['name']} -nb {_ds['#cls']} -m {_model} -algo {_algo} -gr {nc[_nc]}  -jr {_jr} --top_k {_top_k} -sc {_sc} -bt 0.001 -go train -fceal --log --balance --noniid --alpha_dirich {_alpha} -did {gpu}\n"
                 )
             
-            #balance
-            for _nc, _model, _jr, _top_k, _sc in itertools.product(nc, _ds['-m'], join_ratio, top_k, sc):
-                cmd_lst.append(
-                    f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -data {_ds['name']} -nb {_ds['#cls']} -m {_model} -algo {_algo} -gr {nc[_nc]} -jr {_jr} --top_k {_top_k} -sc {_sc} -bt 0.001 -go train -fceal --log --balance -did {gpu}\n"
-                )
             
             with open(run_file, mode='w') as file:
                 file.writelines(
