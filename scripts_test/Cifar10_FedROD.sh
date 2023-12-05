bf5cbbfb377fb1a9f33ad12debc4c56e4c2b5903
Skydvn <mduongbkhn@gmail.com>
2023-12-05 12:40:18 +0900

    update

--- scripts_test/Cifar10_FedROD.sh
+++
@@ -1,2 +0,0 @@
-cd ../system/
-python -u main.py -lbs 16 -nc 100 -jr 1 -data Cifar10 -nb 10 -m cnn20 -algo FedROD -gr 800  -jr 1 --top_k 2 -sc 0 -bt 0.001 -go train -fceal --log --balance --noniid --alpha_dirich 0.1 -did 1
